ded692d71ac3fd970484d60a3932bed200599aa5
Oscar Boykin <oscar@stripe.com>
2017-08-23 00:10:26 -0700

    Use Memoize in Expr.evaluate

--- core/src/main/scala/com/stripe/dagon/Expr.scala
+++ core/src/main/scala/com/stripe/dagon/Expr.scala
@@ -59,28 +59,26 @@ object Expr {
     arg2: Id[T2],
     fn: (N[T1], N[T2]) => N[T3]) extends Expr[N, T3]
 
+  /**
+   * Evaluate the given expression with the given mapping of Id to Expr.
+   */
   def evaluate[N[_], T](idToExp: HMap[Id, Expr[N, ?]], expr: Expr[N, T]): N[T] =
-    evaluate(idToExp, HMap.empty[Expr[N, ?], N], expr)._2
+    evaluateMemo(idToExp)(expr)
 
-  private def evaluate[N[_], T](idToExp: HMap[Id, Expr[N, ?]],
-    cache: HMap[Expr[N, ?], N],
-    expr: Expr[N, T]): (HMap[Expr[N, ?], N], N[T]) = cache.get(expr) match {
-    case Some(node) => (cache, node)
-    case None => expr match {
-      case Const(n) =>
-        (cache + (expr -> n), n)
-      case Var(id) =>
-        val (c1, n) = evaluate(idToExp, cache, idToExp(id))
-        (c1 + (expr -> n), n)
-      case Unary(id, fn) =>
-        val (c1, n1) = evaluate(idToExp, cache, idToExp(id))
-        val n2 = fn(n1)
-        (c1 + (expr -> n2), n2)
-      case Binary(id1, id2, fn) =>
-        val (c1, n1) = evaluate(idToExp, cache, idToExp(id1))
-        val (c2, n2) = evaluate(idToExp, c1, idToExp(id2))
-        val n3 = fn(n1, n2)
-        (c2 + (expr -> n3), n3)
-    }
-  }
+  /**
+   * Build a memoized FunctionK for this particular idToExp. Clearly, this
+   * FunctionK is only valid for the given idToExp which is captured in this
+   * closure.
+   */
+  def evaluateMemo[N[_]](idToExp: HMap[Id, Expr[N, ?]]): FunctionK[Expr[N, ?], N] =
+    Memoize.functionK[Expr[N, ?], N](new Memoize.RecursiveK[Expr[N, ?], N] {
+      def toFunction[T] = {
+        case (Const(n), _) => n
+        case (Var(id), rec) => rec(idToExp(id))
+        case (Unary(id, fn), rec) =>
+          fn(rec(idToExp(id)))
+        case (Binary(id1, id2, fn), rec) =>
+          fn(rec(idToExp(id1)), rec(idToExp(id2)))
+      }
+    })
 }
