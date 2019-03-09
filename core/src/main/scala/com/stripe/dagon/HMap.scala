3f32a957121df8d5f4e710081cd7d05914c8942a
agorobets <alexey.gorobets.v@gmail.com>
2019-03-09 20:06:35 +0300

    rewrite mapValues to strict

--- core/src/main/scala/com/stripe/dagon/HMap.scala
+++ core/src/main/scala/com/stripe/dagon/HMap.scala
@@ -83,7 +83,7 @@ final class HMap[K[_], V[_]](protected val map: Map[K[_], V[_]]) extends Seriali
   }
 
   def mapValues[V1[_]](f: FunctionK[V, V1]): HMap[K, V1] =
-    HMap.from[K, V1](map.mapValues(f(_)))
+    HMap.from[K, V1](map.map { case (k,v) => k -> f(v) }.toMap)
 }
 
 object HMap {
